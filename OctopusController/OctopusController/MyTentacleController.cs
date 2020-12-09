057512c5159b8bf228d654d5893f1470d8e82510
Victor Blas <victor.blas@enti.cat>
2020-12-09 17:09:30 +0100

    Tentacles Done

--- OctopusController/OctopusController/MyTentacleController.cs
+++ OctopusController/OctopusController/MyTentacleController.cs
@@ -21,6 +21,7 @@ namespace OctopusController
         Transform[] _endEffectorSphere;
 
         public Transform[] Bones { get => _bones; }
+        public Transform[] EndEffector { get => _endEffectorSphere; }
 
         //Exercise 1.
         public Transform[] LoadTentacleJoints(Transform root, TentacleMode mode)
@@ -37,10 +38,24 @@ namespace OctopusController
                     //TODO: in _endEffectorsphere you keep a reference to the red sphere 
                     break;
                 case TentacleMode.TENTACLE:
+                    _bones = new Transform[50];
+                    //avançem fins a bone_50
+                    root = root.GetChild(0);
+                    root = root.GetChild(0);
+                    root = root.GetChild(0);
+                    for (int i=0; i <_bones.Length; i++)
+                    {
+                        _bones[i] = root;
+                        root = root.GetChild(0);
+                    }
+                    _endEffectorSphere = new Transform[1];
+                    _endEffectorSphere[0] = root;
                     //TODO: in _endEffectorphere you  keep a reference to the sphere with a collider attached to the endEffector
                     break;
             }
             return Bones;
         }
+
+        
     }
 }
