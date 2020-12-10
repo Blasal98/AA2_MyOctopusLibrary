18916c844b4dd3afb83988ba84f90a8a8045d12e
Victor Blas <victor.blas@enti.cat>
2020-12-10 18:30:53 +0100

    Checkpoint - Start

--- OctopusController/OctopusController/MyScorpionController.cs
+++ OctopusController/OctopusController/MyScorpionController.cs
@@ -16,6 +16,11 @@ namespace OctopusController
         MyTentacleController _tail;
         float animationRange;
 
+        float[] _solutionsTail;
+        Vector3[] _axisTail;
+        Vector3[] _offsetTail;
+
+
         //LEGS
         Transform[] legTargets;
         Transform[] legFutureBases;
@@ -41,12 +46,28 @@ namespace OctopusController
             _tail = new MyTentacleController();
             _tail.LoadTentacleJoints(TailBase, TentacleMode.TAIL);
             //TODO: Initialize anything needed for the Gradient Descent implementation
+            _solutionsTail = new float[_tail.Bones.Length];
+            _axisTail = new Vector3[_tail.Bones.Length];
+            _offsetTail = new Vector3[_tail.Bones.Length];
+
+            for (int i = 0; i < _tail.Bones.Length; i++)
+            {
+                _tail.Bones[0].rotation = Quaternion.identity;
+            }
+            for (int i = 0; i < _tail.Bones.Length; i++)
+            {
+                _solutionsTail[i] = 0;
+                _axisTail[i] = new Vector3(1,0,0);
+                if (i < _tail.Bones.Length - 1) _offsetTail[i] = _tail.Bones[i + 1].position - _tail.Bones[i].position;
+                else _offsetTail[i] = _tail.EndEffector[0].position - _tail.Bones[i].position;
+            }
+
         }
 
         //TODO: Check when to start the animation towards target and implement Gradient Descent method to move the joints.
         public void NotifyTailTarget(Transform target)
         {
-
+            tailTarget = target;
         }
 
         //TODO: Notifies the start of the walking animation
@@ -57,9 +78,9 @@ namespace OctopusController
 
         //TODO: create the apropiate animations and update the IK from the legs and tail
 
-        public void UpdateIK()
+        public void UpdateIK()//la que update unity
         {
- 
+            updateTail();
         }
         #endregion
 
@@ -74,12 +95,40 @@ namespace OctopusController
         //TODO: implement Gradient Descent method to move tail if necessary
         private void updateTail()
         {
+            FKTail(_solutionsTail,_axisTail);
+            if ((_tail.EndEffector[0].position - tailTarget.position).magnitude < 1f)
+            {
+               //TODO
 
+
+            }
         }
         //TODO: implement fabrik method to move legs 
         private void updateLegs()
         {
 
+        }
+        private void gradientFunction(Vector3 target, float[] solutions, int i, float delta)
+        {
+            float gradient = 0;
+        }
+        //aixo es basicament forward kinematics, pero podriem fer punter de funcio
+        private void ErrorFunction(float[] solutions, Vector3[] axis, Vector3[] offsets) 
+        {
+            Vector3 prevPoint = _tail.Bones[0].transform.position;
+            Quaternion rotation = _tail.Bones[0].transform.rotation;
+
+            for(int i = 1; i < _tail.Bones.Length; i++)
+            {
+                rotation *= Quaternion.AngleAxis(solutions[i - 1], axis[i - 1]);
+                Vector3 nextPoint = prevPoint + rotation * offsets[i];
+                prevPoint = nextPoint;
+            }
+            
+        }
+        private void FKTail(float[] solutions, Vector3[] axis)
+        {
+            
         }
         #endregion
     }
