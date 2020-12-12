8b9be5f46ce9078844fba8644c6df652a54fa050
Victor Blas <victor.blas@enti.cat>
2020-12-12 19:00:57 +0100

    Neteja i Comentaris

--- OctopusController/OctopusController/MyOctopusController.cs
+++ OctopusController/OctopusController/MyOctopusController.cs
@@ -30,8 +30,6 @@ namespace OctopusController
         public float SwingMin {  set => _swingMin = value; }
         public float SwingMax { set => _swingMax = value; }
 
-        bool aux = false;
-
         public void TestLogging(string objectName)
         {
 
@@ -80,94 +78,65 @@ namespace OctopusController
             
         }
 
-
-
-
         #endregion
 
 
         #region private and internal methods
-        //todo: add here anything that you need
 
         void update_ccd()
         {
-
-            //_tentacles[0].Bones[30].transform.Rotate(new Vector3(1,0,1).normalized, 1);
-            if (!aux)
-            {
                 
-                //aux = true;
-                for (int i = 0; i < _tentacles.Length; i++) //recorrem cada tentacle
+            for (int i = 0; i < _tentacles.Length; i++) //recorrem cada tentacle
+            {
+                bool done = false;
+                float rotationAngle;
+                float cos;
+                float error = 0.1f;
+                int tries = 0;
+
+                while (!done && tries < 10) //si compleix el nostre criteri d'error o si supera limit de calculs (10)
                 {
-                    bool done = false;
-                    float[] theta = new float[_tentacles[i].Bones.Length];
-                    float[] sin = new float[_tentacles[i].Bones.Length];
-                    float[] cos = new float[_tentacles[i].Bones.Length];
-                    float epsilon = 0.1f;
-                    int tries = 0;
-                    //_tentacles[i].Bones[49].transform.position = _randomTargets[i].position;
-                    while (!done && tries < 10)
+                    for (int j = _tentacles[i].Bones.Length - 1; j >= 0; j--) //recorrem cada bone desde l'ultim
                     {
+
+                        Vector3 E_R = _tentacles[i].EndEffector[0].transform.position - _tentacles[i].Bones[j].transform.position; //vector de posicio de joint a endeffector
+                        Vector3 T_R = _randomTargets[i].transform.position - _tentacles[i].Bones[j].transform.position; //vector de posicio de joint al target
+
+                        if (E_R.magnitude * T_R.magnitude <= 0.001f) //evitem valors petits per no tenir valors massa grans
+                            cos = 1;
                         
-                        for (int j = _tentacles[i].Bones.Length - 1; j >= 0; j--) //recorrem cada bone desde l'ultim
-                        {
+                        else //sino u calculem com u fariem normalment -> aillar cos de la formula de dot product
+                            cos = Vector3.Dot(E_R, T_R) / (E_R.magnitude * T_R.magnitude);
 
-                            Vector3 E_R = _tentacles[i].EndEffector[0].transform.position - _tentacles[i].Bones[j].transform.position;
-                            Vector3 T_R = _randomTargets[i].transform.position - _tentacles[i].Bones[j].transform.position;
+                        rotationAngle = Mathf.Acos(Mathf.Max(-1, Mathf.Min(1, cos))); //garanteix que acos operi amb argument entre -1 i 1
+                        rotationAngle = (float)angleBetweenPIAndMinusPI(rotationAngle) * Mathf.Rad2Deg; //acotar l'angle entre -PI i PI ->anar pel cami curt-> i posaro en degrees
 
-                            if (E_R.magnitude * T_R.magnitude <= 0.001f)
-                            {
-                                // cos component will be 1 and sin will be 0
-                                cos[j] = 1;
-                                sin[j] = 0;
-                            }
-                            else
-                            {
-                                // find the components using dot and cross product
-                                cos[j] = Vector3.Dot(E_R, T_R) / (E_R.magnitude * T_R.magnitude);
-                                sin[j] = (Vector3.Cross(E_R, T_R)).magnitude / (E_R.magnitude * T_R.magnitude);
-
-                            }
-                            theta[j] = Mathf.Acos(Mathf.Max(-1, Mathf.Min(1, cos[j])));
-                            if (sin[j] < 0.0f)
-                                theta[j] = -theta[j];
-                            theta[j] = (float)SimpleAngle(theta[j]) * Mathf.Rad2Deg;
-
-                            //Quaternion q = Quaternion.FromToRotation(E_R.normalized, T_R.normalized);
-                            _tentacles[i].Bones[j].Rotate(Vector3.Cross(E_R,T_R).normalized,theta[j],Space.World);
+                        _tentacles[i].Bones[j].Rotate(Vector3.Cross(E_R,T_R).normalized,rotationAngle,Space.World); //aplicar rotacio
                                 
-                        }
-                        tries++;
-                        float x = Mathf.Abs(_tentacles[i].EndEffector[0].transform.position.x - _randomTargets[i].transform.position.x);
-                        float y = Mathf.Abs(_tentacles[i].EndEffector[0].transform.position.y - _randomTargets[i].transform.position.y);
-                        float z = Mathf.Abs(_tentacles[i].EndEffector[0].transform.position.z - _randomTargets[i].transform.position.z);
-
-                        // if target is within reach (within epsilon) then the process is done
-                        if (x < epsilon && y < epsilon && z < epsilon)
-                        {
-                            done = true;
-                        }
-                        // if it isn't, then the process should be repeated
-                        else
-                        {
-                            done = false;
-                            
-                        }
-                        
                     }
-                }
+                    tries++; //controlem quantes iteracions portem per no superar el limit de calculs establert (10)
 
+                    //calculem distancia entre endeffector i target per a cada component del eix de coordenades
+                    float x = Mathf.Abs(_tentacles[i].EndEffector[0].transform.position.x - _randomTargets[i].transform.position.x); 
+                    float y = Mathf.Abs(_tentacles[i].EndEffector[0].transform.position.y - _randomTargets[i].transform.position.y);
+                    float z = Mathf.Abs(_tentacles[i].EndEffector[0].transform.position.z - _randomTargets[i].transform.position.z);
+
+                    if (x < error && y < error && z < error) //si la distancia a cada component es menor que el nostre error hem acabat
+                        done = true;
+                }
             }
+
+            
         }
 
-        double SimpleAngle(double theta)
+        double angleBetweenPIAndMinusPI(double angle)
         {
-            theta = theta % (2.0 * Mathf.PI);
-            if (theta < -Mathf.PI)
-                theta += 2.0 * Mathf.PI;
-            else if (theta > Mathf.PI)
-                theta -= 2.0 * Mathf.PI;
-            return theta;
+            angle = angle % (2.0 * Mathf.PI);
+            if (angle < -Mathf.PI)
+                angle += 2.0 * Mathf.PI;
+            else if (angle > Mathf.PI)
+                angle -= 2.0 * Mathf.PI;
+            return angle;
         }
 
         #endregion
